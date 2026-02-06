399d4b4e0f523f0fdfee8a0d6f9aaf405e88c4d4
Youcef <youcefcopy@gmail.com>
2026-02-06 11:44:52 +0100

    adding the color pallete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import AppNav from './components/AppNav.vue'
+</script>
 
 <template>
-  <div class="css-lab"></div>
+  <div class="css-lab">
+    <AppNav />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
-<template>pass</template>
+<template>
+  <nav>
+    <h2 class="logo"><span>css</span>lab</h2>
+    <i class="fa-solid fa-bars mobile-only"></i>
+    <div class="headers">
+      <a href="#">Buttons</a>
+      <a href="#">Text Input</a>
+      <a href="#">Animations</a>
+      <a href="#">Menu Icons</a>
+      <a href="#">How to</a>
+    </div>
+    <button>Blog</button>
+  </nav>
+</template>
 
 <style scoped></style>

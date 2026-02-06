ba0026a7220aeb83a71d715b3c3b2d330bd01e27
Youcef <youcefcopy@gmail.com>
2026-02-06 15:30:40 +0100

    font awesome has been installed

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -3,7 +3,8 @@
 <template>
   <nav>
     <h2 class="logo"><span>css</span>lab</h2>
-    <i class="fa-solid fa-bars mobile-only"></i>
+    <i class="fa-solid fa-bars"></i>
+    <!-- <i class="fa-solid fa-bars mobile-only"></i> -->
     <div class="headers">
       <a href="#">Buttons</a>
       <a href="#">Text Input</a>

402eb693c034f592ee8afdad25e217d48d3a49ae
Youcef <youcefcopy@gmail.com>
2026-02-06 11:46:25 +0100

    the navbar has been styled

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,12 @@ import AppNav from './components/AppNav.vue'
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.css-lab {
+  background-color: var(--brand-color);
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -15,4 +15,51 @@
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: clamp(0.75rem, 2vw, 1.5rem) clamp(1rem, 4vw, 3rem);
+}
+nav .logo {
+  font-size: 2rem;
+  font-weight: 600;
+  color: var(--text-color);
+  padding: 1rem 2rem;
+  cursor: pointer;
+}
+
+nav .logo span {
+  color: var(--secondary-color);
+}
+
+nav .headers a {
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: var(--text-color);
+  margin: 1rem;
+  transition: 0.2s;
+}
+
+nav .headers a:hover {
+  color: var(--text-hover);
+}
+
+button {
+  background-color: var(--secondary-color);
+  color: var(--brand-color);
+  font-size: 1.2rem;
+  font-weight: 600;
+  padding: 12px 16px;
+  border: none;
+  border-radius: 0.8rem;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+button:hover {
+  background-color: var(--secondary-hover);
+}
+</style>

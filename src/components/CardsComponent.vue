2b5166afa18b6729b76d2866469fb7bbd9cf619e
Youcef <youcefcopy@gmail.com>
2026-02-10 10:05:25 +0100

    defining the props object in the container component

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  card: {
+    type: Object,
+  },
+})
+</script>
 
 <template>this is the card component itslef</template>
 

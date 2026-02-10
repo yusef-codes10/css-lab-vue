d372bb16a8fb48861ababcfac98a4c7076d431de
Youcef <youcefcopy@gmail.com>
2026-02-10 09:59:14 +0100

    data js has been added

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+// adding the cards dynamically is way better
+const cards = [
+  {
+    id: 'card1',
+    langsUsed: ['html', 'css'],
+    cardTitle: 'Retro Button',
+    cardDiscription: 'View a cool retro button',
+    girdArea: 'button',
+  },
+  {
+    id: 'navbar1',
+    langsUsed: ['html', 'css'],
+    cardTitle: 'Navbar',
+    cardDiscription: 'Modern navabr layout',
+    girdArea: 'animation',
+  },
+  {
+    id: 'navbar2',
+    langsUsed: ['html', 'css', 'js'],
+    cardTitle: 'Navbar',
+    cardDiscription: 'Responsive Navbar',
+    girdArea: 'icon',
+  },
+  {
+    id: 'animation',
+    langsUsed: ['css'],
+    cardTitle: 'Animation',
+    cardDiscription:
+      'View a variety of awesome CSS3 Animations that you can edit and slot right into your next website!',
+    girdArea: 'textinput',
+  },
+  {
+    id: 'text-input',
+    langsUsed: ['html', 'css'],
+    cardTitle: 'Text Input',
+    cardDiscription:
+      "View a selection of text input forms ranging from text inputs, textarea's, radio buttons, and custom dropdown menus. Many components include custom focus, hover & active states!",
+    girdArea: 'modal',
+  },
+  {
+    id: 'miscellaneous',
+    langsUsed: ['html', 'css', 'js'],
+    cardTitle: 'Miscellaneous',
+    cardDiscription:
+      "A random selection of components that didn't quite fit in any of the other categories!",
+    girdArea: 'miscellaneous',
+  },
+]
+
+export { cards }

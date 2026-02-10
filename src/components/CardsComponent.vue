b765cd08b51d80241e9f96332eba30e34c12931b
Youcef <youcefcopy@gmail.com>
2026-02-10 10:02:48 +0100

    cardsContainer & cardComponent have been created

---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>this is the card component itslef</template>
+
+<style scoped></style>
---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>this is where we use v-for to laod content</template>
+
+<style scoped></style>

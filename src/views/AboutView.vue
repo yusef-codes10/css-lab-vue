f0f60f55865060b7d6b613866d0f649ab0f85e6e
Youcef <youcefcopy@gmail.com>
2026-02-18 16:27:37 +0100

    creating the views

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>This is the about view</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>This is the home view</template>
+
+<style></style>

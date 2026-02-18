8de76bbb572673bd27d9cf71ea6f824454b373a7
Youcef <youcefcopy@gmail.com>
2026-02-18 16:37:16 +0100

    register the router in main.js | same as pinia store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router/index.js'
 import './assets/global.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// * register the router
+app.use(router)
+
+app.mount('#app')

777643c92e310917fb94f8caacb49f366af4b54c
Youcef <youcefcopy@gmail.com>
2026-02-19 12:36:02 +0100

    2- add the buttons to the routes as a route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import ButtonsView from '@/views/ButtonsView.vue'
 
 // * the routes we define
 const routes = [
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/buttons',
+    name: 'buttons',
+    component: ButtonsView,
   },
   {
     path: '/about',

c33e28f401808cdb99c9ba92afff53d49c177a65
Yusef Codes <youcefcopy@gmail.com>
2026-02-26 15:18:08 +0100

    importing the 2nd button

--- src/views/ButtonsView.vue
+++ src/views/ButtonsView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import RetroButton from '@/components/buttons/RetroButton.vue'
+import ShadowButton from '@/components/buttons/shadowButton.vue';
 </script>
 
 <template>
   <div class="buttons">
     This is the buttons view
     <RetroButton />
+    <ShadowButton />
   </div>
 </template>
 
